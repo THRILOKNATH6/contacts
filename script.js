9cb4167c0782011084ecef065da492d1f096cfcb
THRILOKNATH6 <52757771+THRILOKNATH6@users.noreply.github.com>
2020-04-06 19:34:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ jQuery(function ($) {
         // initialize playlist and controls
         var index = 0,
             playing = false,
-            mediaPath ='contacts/Songs/',
+            mediaPath = 'https://github.com/THRILOKNATH6/thrilok1/Songs/',
             extension = '',
             tracks = [{
                 "track": 1,
