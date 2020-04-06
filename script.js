7c830bdcdc3233e3d298536890ec985d34933ffc
THRILOKNATH6 <52757771+THRILOKNATH6@users.noreply.github.com>
2020-04-06 19:22:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ jQuery(function ($) {
         // initialize playlist and controls
         var index = 0,
             playing = false,
-            mediaPath ='Songs/',
+            mediaPath ='contacts/Songs/',
             extension = '',
             tracks = [{
                 "track": 1,
